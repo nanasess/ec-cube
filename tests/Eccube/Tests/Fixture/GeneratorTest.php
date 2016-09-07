40aaca12d48c271bc295f733fedf22f686d9097b
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-09-07 18:28:45 +0900

    Add Fixture Generator
    execute command
    
    ```
    ./vendor/bin/phpunit tests/Eccube/Tests/Fixture/GeneratorTest.php
    ```

---
+++ tests/Eccube/Tests/Fixture/GeneratorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Eccube\Tests\Fixture;
+
+use Eccube\Common\Constant;
+use Eccube\Tests\EccubeTestCase;
+
+/**
+ * Fixture Generator
+ *
+ * @author Kentaro Ohkouchi
+ */
+class GeneratorTest extends EccubeTestCase
+{
+    protected $numberOfProducts = 100;
+    protected $numberOfCustomer = 100;
+    protected $numberOfOrder = 10;
+    protected $numberOfDeliveries = 5;
+    protected $Customers;
+    protected $Products;
+    protected $Deliveries;
+    protected $Orders;
+
+    public function setUp()
+    {
+        // parent::setUp();
+        $this->app = $this->createApplication();
+        $this->Customers = array();
+        $this->Products = array();
+        $this->Deliveries = array();
+        $this->Orders = array();
+    }
+
+    public function tearDown()
+    {
+        // parent::tearDown();
+    }
+
+    public function testDeliveryGenerator()
+    {
+        for ($i = 0; $i < $this->numberOfDeliveries; $i++) {
+            $this->Deliveries[] = $this->app['eccube.fixture.generator']->createDelivery();
+        }
+        $this->assertEquals($this->numberOfDeliveries, count($this->Deliveries));
+    }
+
+    public function testProductGenerator()
+    {
+        for ($i = 0; $i < $this->numberOfProducts; $i++) {
+            $this->Products[] = $this->app['eccube.fixture.generator']->createProduct();
+        }
+        $this->assertEquals($this->numberOfProducts, count($this->Products));
+    }
+    public function testCustomerGenerator()
+    {
+        for ($i = 0; $i < $this->numberOfCustomer; $i++) {
+            $this->Customers[] = $this->app['eccube.fixture.generator']->createCustomer();
+        }
+        $this->assertEquals($this->numberOfCustomer, count($this->Customers));
+    }
+
+    public function testOrderGenerator()
+    {
+        $faker = $this->getFaker();
+        $Customers = $this->app['eccube.repository.customer']->findAll();
+        $Deliveries = $this->app['eccube.repository.delivery']->findAll();
+        $Products = $this->app['eccube.repository.product']->findAll();
+        $Status = $this->app['eccube.repository.order_status']->find($this->app['config']['order_new']);
+        foreach ($Customers as $Customer) {
+            $Delivery = $Deliveries[$faker->numberBetween(0, $this->numberOfDeliveries)];
+            $Product = $Products[$faker->numberBetween(0, $this->numberOfProducts)];
+            $charge = $faker->randomNumber(4);
+            $discount = $faker->randomNumber(4);
+            for ($i = 0; $i < $this->numberOfOrder; $i++) {
+                $Order = $this->app['eccube.fixture.generator']->createOrder($Customer, $Product->getProductClasses()->toArray(), $Delivery, $charge, $discount);
+                $Order->setOrderStatus($Status);
+                $Order->setOrderDate(new \DateTime());
+                $this->app['orm.em']->flush($Order);
+                $this->Orders[] = $Order;
+            }
+        }
+        $this->assertEquals(count($Customers) * $this->numberOfOrder, count($this->Orders));
+    }
+}
