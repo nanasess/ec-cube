d86a56e563bd922764de11b3cfc184e57a164af4
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-11-07 14:18:12 +0900

    実際の運用に近いデータを生成するよう修正
    - 毎回実行しなくても良いテストはスキップ
    - cron などでの実行を考慮し $customerCountLimit を追加

--- tests/Eccube/Tests/Fixture/GeneratorTest.php
+++ tests/Eccube/Tests/Fixture/GeneratorTest.php
@@ -18,6 +18,7 @@ class GeneratorTest extends EccubeTestCase
     protected $numberOfOrder = 10;
     protected $numberOfDeliveries = 5;
     protected $numberOfPayments = 3;
+    protected $customerCountLimit = 1000;
     protected $Customers;
     protected $Products;
     protected $Deliveries;
@@ -40,6 +41,7 @@ class GeneratorTest extends EccubeTestCase
 
     public function testDeliveryGenerator()
     {
+        $this->markTestSkipped();
         $Payments = $this->app['eccube.repository.payment']->findAll();
         for ($i = 0; $i < $this->numberOfDeliveries; $i++) {
             $Delivery = $this->app['eccube.fixture.generator']->createDelivery();
@@ -62,6 +64,7 @@ class GeneratorTest extends EccubeTestCase
 
     public function testProductGenerator()
     {
+        $this->markTestSkipped();
         for ($i = 0; $i < $this->numberOfProducts; $i++) {
             $this->Products[] = $this->app['eccube.fixture.generator']->createProduct();
         }
@@ -69,6 +72,7 @@ class GeneratorTest extends EccubeTestCase
     }
     public function testCustomerGenerator()
     {
+        $this->markTestSkipped();
         for ($i = 0; $i < $this->numberOfCustomer; $i++) {
             $this->Customers[] = $this->app['eccube.fixture.generator']->createCustomer();
         }
@@ -77,18 +81,29 @@ class GeneratorTest extends EccubeTestCase
 
     public function testOrderGenerator()
     {
+        $count = $this->getCustomerCount();
+        if ($count > $this->customerCountLimit) {
+            $this->markTestSkipped();
+        }
         $faker = $this->getFaker();
-        $Customers = $this->app['eccube.repository.customer']->findAll();
+        for ($i = 0; $i < $this->numberOfCustomer; $i++) {
+            $email = microtime(true).'.'.$faker->safeEmail;
+            $this->Customers[] = $this->app['eccube.fixture.generator']->createCustomer($email);
+        }
+        for ($i = 0; $i < $this->numberOfProducts; $i++) {
+            $this->Products[] = $this->app['eccube.fixture.generator']->createProduct();
+        }
+        $Customers = $this->Customers;
         $Deliveries = $this->app['eccube.repository.delivery']->findAll();
-        $Products = $this->app['eccube.repository.product']->findAll();
-        $Status = $this->app['eccube.repository.order_status']->find($this->app['config']['order_new']);
+        $Products = $this->Products;
         foreach ($Customers as $Customer) {
-            $Delivery = $Deliveries[$faker->numberBetween(0, $this->numberOfDeliveries)];
-            $Product = $Products[$faker->numberBetween(0, $this->numberOfProducts)];
+            $Delivery = $Deliveries[$faker->numberBetween(0, count($Deliveries) - 1)];
+            $Product = $Products[$faker->numberBetween(0, $this->numberOfProducts - 1)];
             $charge = $faker->randomNumber(4);
             $discount = $faker->randomNumber(4);
             for ($i = 0; $i < $this->numberOfOrder; $i++) {
                 $Order = $this->app['eccube.fixture.generator']->createOrder($Customer, $Product->getProductClasses()->toArray(), $Delivery, $charge, $discount);
+                $Status = $this->app['eccube.repository.order_status']->find($faker->numberBetween(1, 8));
                 $Order->setOrderStatus($Status);
                 $Order->setOrderDate($faker->dateTimeThisYear());
                 $this->app['orm.em']->flush($Order);
@@ -96,5 +111,14 @@ class GeneratorTest extends EccubeTestCase
             }
         }
         $this->assertEquals(count($Customers) * $this->numberOfOrder, count($this->Orders));
+    }
+
+    protected function getCustomerCount()
+    {
+        $qb = $this->app['orm.em']->createQueryBuilder();
+        $qb->select("count(c.id)")
+           ->from("Eccube\\Entity\\Customer", "c");
+        $query = $qb->getQuery();
+        return $query->getSingleScalarResult();
     }
 }
